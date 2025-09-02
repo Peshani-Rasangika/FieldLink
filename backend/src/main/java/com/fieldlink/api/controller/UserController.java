1f9467c1feb63d9a92fb8c4b7a452f33a39cfa1a
Peshani-Rasangika <prranaweeraos2001@gmail.com>
2025-09-02 08:38:10 +0530

    feat: implement user registration and security configuration

---
+++ backend/src/main/java/com/fieldlink/api/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.fieldlink.api.controller;
+
+import com.fieldlink.api.model.User;
+import com.fieldlink.api.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User savedUser = userService.saveUser(user);
+        return ResponseEntity.ok(savedUser);
+    }
+}
